781e8929e7e84c8e1bf480cbfe3f7d6fc1f924bf
Adrian Smith <smith.adriane@gmail.com>
2021-07-17 16:04:48 -0700

    include all src header files in archive.

--- build_windows.sh
+++ build_windows.sh
@@ -39,41 +39,40 @@ gn gen out/${build_type}-x64 --args="${args} \
 ninja -C out/${build_type}-x64 skia modules
 
 if [ "${archive:-false}" = "true" ]; then
-  7z a -tzip -r ../Skia-${release}-windows-${build_type}-x64.zip \
-    out/${build_type}-x64/*.lib \
-    out/${build_type}-x64/icudtl.dat \
-    include \
-    modules/particles/include/*.h \
-    modules/skottie/include/*.h \
-    modules/skparagraph/include/*.h \
-    modules/skplaintexteditor/include/*.h \
-    modules/skresources/include/*.h \
-    modules/sksg/include/*.h \
-    modules/skshaper/include/*.h \
-    modules/skshaper/src/*.h \
-    src/core/*.h \
-    src/gpu/gl/*.h \
-    src/utils/*.h \
-    third_party/externals/angle2/LICENSE \
-    third_party/externals/angle2/include \
-    third_party/externals/freetype/docs/FTL.TXT \
-    third_party/externals/freetype/docs/GPLv2.TXT \
-    third_party/externals/freetype/docs/LICENSE.TXT \
-    third_party/externals/freetype/include \
-    third_party/externals/libpng/LICENSE \
-    third_party/externals/libpng/*.h \
-    third_party/externals/libwebp/COPYING \
-    third_party/externals/libwebp/PATENTS \
-    third_party/externals/libwebp/src/dec/*.h \
-    third_party/externals/libwebp/src/dsp/*.h \
-    third_party/externals/libwebp/src/enc/*.h \
-    third_party/externals/libwebp/src/mux/*.h \
-    third_party/externals/libwebp/src/utils/*.h \
-    third_party/externals/libwebp/src/webp/*.h \
-    third_party/externals/harfbuzz/COPYING \
-    third_party/externals/harfbuzz/src/*.h \
-    third_party/externals/swiftshader/LICENSE.txt \
-    third_party/externals/swiftshader/include \
-    third_party/externals/zlib/LICENSE \
-    third_party/externals/zlib/*.h
+    find src -name '*.h' -print0 | \
+        xargs -0 \
+              7z a -tzip -r ../Skia-${release}-windows-${build_type}-x64.zip \
+              out/${build_type}-x64/*.lib \
+              out/${build_type}-x64/icudtl.dat \
+              include \
+              modules/particles/include/*.h \
+              modules/skottie/include/*.h \
+              modules/skparagraph/include/*.h \
+              modules/skplaintexteditor/include/*.h \
+              modules/skresources/include/*.h \
+              modules/sksg/include/*.h \
+              modules/skshaper/include/*.h \
+              modules/skshaper/src/*.h \
+              third_party/externals/angle2/LICENSE \
+              third_party/externals/angle2/include \
+              third_party/externals/freetype/docs/FTL.TXT \
+              third_party/externals/freetype/docs/GPLv2.TXT \
+              third_party/externals/freetype/docs/LICENSE.TXT \
+              third_party/externals/freetype/include \
+              third_party/externals/libpng/LICENSE \
+              third_party/externals/libpng/*.h \
+              third_party/externals/libwebp/COPYING \
+              third_party/externals/libwebp/PATENTS \
+              third_party/externals/libwebp/src/dec/*.h \
+              third_party/externals/libwebp/src/dsp/*.h \
+              third_party/externals/libwebp/src/enc/*.h \
+              third_party/externals/libwebp/src/mux/*.h \
+              third_party/externals/libwebp/src/utils/*.h \
+              third_party/externals/libwebp/src/webp/*.h \
+              third_party/externals/harfbuzz/COPYING \
+              third_party/externals/harfbuzz/src/*.h \
+              third_party/externals/swiftshader/LICENSE.txt \
+              third_party/externals/swiftshader/include \
+              third_party/externals/zlib/LICENSE \
+              third_party/externals/zlib/*.h
 fi
